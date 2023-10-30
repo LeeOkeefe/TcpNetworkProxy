07bb4784f9cc10ac6014b3c646555b1a70ff1126
Lee <leeokeefe94@gmail.com>
2023-10-30 15:14:25 +0000

    Project setup

---
+++ TcpNetworkProxy/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace TcpNetworkProxy;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
