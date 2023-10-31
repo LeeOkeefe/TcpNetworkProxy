cb7d96e728d10b9a3247ad3fefa0044ec0af1527
Lee <leeokeefe94@gmail.com>
2023-10-31 16:59:49 +0000

    Create network page with dummy data

--- TcpNetworkProxy/Data/NetworkEntry.cs
+++ TcpNetworkProxy/Data/NetworkEntry.cs
@@ -2,12 +2,14 @@ namespace TcpNetworkProxy.Data;
 
 public sealed class NetworkEntry
 {
+    public TimeOnly Timestamp { get; }
     public string Source { get; }
     public string Destination { get; }
     public string Data { get; }
     
-    public NetworkEntry(string source, string destination, string data)
+    public NetworkEntry(TimeOnly timestamp, string source, string destination, string data)
     {
+        Timestamp = timestamp;
         Source = source ?? throw new ArgumentNullException(nameof(source));
         Destination = destination ?? throw new ArgumentNullException(nameof(destination));
         Data = data ?? throw new ArgumentNullException(nameof(data));
